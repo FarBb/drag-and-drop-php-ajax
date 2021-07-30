9ec10a0ef9c060722be5d09de56d3bcaf5b9a305
FarBb <loveinsemarang@gmail.com>
2021-07-30 14:35:44 +0700

    Drag And Drop Ajax php

---
+++ index.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="style.css" />
+<div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
+    <div id="drag_upload_file">
+        <p>Drop file here</p>
+        <p>or</p>
+        <p><input type="button" value="Select File" onclick="file_explorer();" /></p>
+        <input type="file" id="selectfile" />
+    </div>
+</div>
+<div class="img-content"></div>
+<script src="costum.js"></script>
